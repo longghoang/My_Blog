1dd3357b4a5a3235b202ff88cc8c67b1447ce05f
longghoang <nguyenlonglqmb@gmail.com>
2023-06-30 08:02:50 +0700

    Pagination, custom UI

--- src/app/controllers/newsController.js
+++ src/app/controllers/newsController.js
@@ -4,21 +4,43 @@ const moment = require('moment');
 class NewsController {
   async index(req, res, next) {
     try {
-      const blogPosts = await BlogPost.find({}).exec(); // Thực hiện truy vấn và lấy kết quả
-      const Blog = blogPosts.map(blogPost => {
-        const createdDate = moment(blogPost.created_at).format('HH:mm:ss  ,  DD/MM/YYYY');
-        return { ...blogPost.toObject(), createdDate };
-      }); // tObject
+      const pageNumber = req.query.page || 1; // Trang hiện tại, mặc định là 1
+      const pageSize = 6; // Số lượng bài viết trên mỗi trang
 
-      res.render('home', {
-        Blog: Blog, // Truyền kết quả truy vấn vào hàm res.render()
+      
+      const totalPosts = await BlogPost.countDocuments();
+      const totalPages = Math.ceil(totalPosts / pageSize);
+
+      const blogPosts = await BlogPost.find()
+        .skip((pageNumber - 1) * pageSize)
+        .limit(pageSize)
+        .exec();
+
+      const Blog = blogPosts.map(blogPost => {
+        const createdDate = moment(blogPost.created_at).format('HH:mm:ss, DD/MM/YYYY');
+        return { ...blogPost.toObject(), createdDate };
       });
 
+      // Tạo mảng pages
+      const pages = [];
+      for (let i = 1; i <= totalPages; i++) {
+        pages.push({
+          pageNumber: i,
+          isCurrent: i === pageNumber,
+        });
+      }
+
+     
+      res.render('home', {
+        Blog: Blog,
+        pages: pages,
+      });
     } catch (error) {
       next(error);
     }
   }
 
+///
   async read(req, res, next) {
     try {
       const postId = req.query._id;
